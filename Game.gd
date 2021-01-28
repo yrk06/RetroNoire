238f9a4badea94c1b30518a7e384a889e29c2a9a
Yerik <yerik.koslowski@gmail.com>
2021-01-28 19:59:29 -0300

    Adicionado mudanças para teste da API de som

--- Game.gd
+++ Game.gd
@@ -12,14 +12,24 @@ func _ready():
 	Fmod.init(1024, Fmod.FMOD_STUDIO_INIT_LIVEUPDATE, Fmod.FMOD_INIT_NORMAL)
 	
 	# load banks
-	Fmod.load_bank("res://assets/Master.strings.bank", Fmod.FMOD_STUDIO_LOAD_BANK_NORMAL)
-	Fmod.load_bank("res://assets/Master.bank", Fmod.FMOD_STUDIO_LOAD_BANK_NORMAL)
+	Fmod.load_bank("res://assets/testAssets/Master.strings.bank", Fmod.FMOD_STUDIO_LOAD_BANK_NORMAL)
+	Fmod.load_bank("res://assets/testAssets/Master.bank", Fmod.FMOD_STUDIO_LOAD_BANK_NORMAL)
 	
 	## Descomentar essa linha faz o jogo não rodar (?)
 	Fmod.add_listener(0, $Node2D)
 	
 	## Tocar os eventos
-	#Fmod.play_one_shot("event:/mx_menu", self)
+	var event = Fmod.create_event_instance("event:/mx_dynamic")
+	Fmod.start_event(event)
+	
+	var tension = 0
+	while tension <= 1:
+		Fmod.set_global_parameter_by_name("mx_tension",tension)
+		tension += 0.1
+		yield(get_tree().create_timer(2),"timeout")
+		
+	Fmod.stop_event(event,Fmod.FMOD_STUDIO_STOP_ALLOWFADEOUT)
+	Fmod.release_event(event)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
