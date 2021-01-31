4f5393ec5a9b217843ba928cf68551ba34a1971d
Renata Cavalheiro da Silva <recavalheiro89@gmail.com>
2021-01-31 01:50:25 -0300

    adicionando lista de casos para selecionar

--- Menu/RetornarAoMenu.gd
+++ Menu/RetornarAoMenu.gd
@@ -1,4 +1,16 @@
 extends Control
 
+# var cases = get(res://case_files/) <- alterar a chamada para os casos
+
 func _on_Button_pressed():
 	get_tree().change_scene('res://Menu/MenuInicial.tscn')
+
+func _on_OptionButton_pressed():
+	var select_case = get_node("CenterContainer/VBoxContainer/OptionButton")
+	pass
+	# for item in cases:
+	#	select_case.add_item(item) <- adicionar os casos na lista
+
+func _on_OptionButton_item_selected(index):
+	pass
+	# get_tree().change_scene(index)
