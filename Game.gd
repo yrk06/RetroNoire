134e1c1e48b7f51d1c9c8636310c80964409f6c6
Yerik <yerik.koslowski@gmail.com>
2021-01-31 02:30:57 -0300

    Adicionado lógica de save/load e musica

--- Game.gd
+++ Game.gd
@@ -7,15 +7,23 @@ extends Node
 var case_NPCs = []
 var case_Pistas = []
 
+var case_logic_relay = []
+var case_mx_areas = []
+
+var current_case_name = 'case_Frederico'
+
 var npcObject = preload("res://Object/NPCsObject.gd")
 var pistaObject = preload("res://Object/PistasObject.gd")
 
 var mainWorld = preload("res://Maps/WorldMap.tscn")
+var LogicRelay = preload("res://addons/LogicConectors/LogicRelay.gd")
+
+var mx_gameplay
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	
 	print(get_file_paths_in_folder('res://assets/testAssets/'))
-	load_case_file('case_002')
+	load_case_file(current_case_name)
 	##Sound Listener
 	
 	var listener = Node2D.new()
@@ -24,21 +32,22 @@ func _ready():
 	Fmod.init(1024, Fmod.FMOD_STUDIO_INIT_LIVEUPDATE, Fmod.FMOD_INIT_NORMAL)
 	
 	# load banks
-	Fmod.load_bank("res://assets/testAssets/Master.strings.bank", Fmod.FMOD_STUDIO_LOAD_BANK_NORMAL)
-	Fmod.load_bank("res://assets/testAssets/Master.bank", Fmod.FMOD_STUDIO_LOAD_BANK_NORMAL)
+	Fmod.load_bank("res://Sound/Master.strings.bank", Fmod.FMOD_STUDIO_LOAD_BANK_NORMAL)
+	Fmod.load_bank("res://Sound/Master.bank", Fmod.FMOD_STUDIO_LOAD_BANK_NORMAL)
 	
 	##Fmod.add_listener(0, $Node2D)
 	
 	## Tocar os eventos
-	var mx_gameplay = Fmod.create_event_instance("event:/mx_gameplay_dynamic")
-	Fmod.start_event(mx_gameplay)
-	Fmod.set_event_volume(mx_gameplay,0.25)
+	mx_gameplay = Fmod.create_event_instance("event:/mx_gameplay_dynamic")
+	#Fmod.start_event(mx_gameplay)
+	Fmod.set_event_volume(mx_gameplay,0.5)
 	
 	var bg_noite = Fmod.create_event_instance("event:/bg_noite") 
 	Fmod.start_event(bg_noite)
 
 func enter_door(door):
 	## we need to load interior?
+	yield(UiInterface.fadein(),'fade_finished')
 	var to_interior = door.to_interior
 	
 	if to_interior:
@@ -60,25 +69,27 @@ func enter_door(door):
 			c.queue_free()
 			yield(c,"tree_exited")
 		
-		##Position Player
-		get_node('/root/MainTree/Player').global_position = door.local_position_entry
+	else:
+		## Check is world map is loaded
+		##Unload any possible interiors
+		for c in get_node('/root/MainTree/Interior').get_children():
+			c.queue_free()
 		
-		try_loading_npcs()
-		try_loading_pistas()
-		return
-	## Check is world map is loaded
-	##Unload any possible interiors
-	for c in get_node('/root/MainTree/Interior').get_children():
-		c.queue_free()
-	
-	##Load World
-	if not len(get_node('/root/MainTree/WorldMapNode').get_children()) > 0:
-		var exterior = mainWorld.instance()
-		get_node('/root/MainTree/WorldMapNode').add_child(exterior)
+		##Load World
+		if not len(get_node('/root/MainTree/WorldMapNode').get_children()) > 0:
+			var exterior = mainWorld.instance()
+			get_node('/root/MainTree/WorldMapNode').add_child(exterior)
 	##Position Player
 	get_node('/root/MainTree/Player').global_position = door.local_position_entry
+	try_loading_all_instances()
+	UiInterface.fadeout()
+	
+func try_loading_all_instances():
 	try_loading_npcs()
 	try_loading_pistas()
+	try_load_logic_relay()
+	try_load_mx_area()
+	
 	
 func try_loading_npcs():
 	for p in case_NPCs:
@@ -102,6 +113,76 @@ func load_case_file(case_name):
 	var pistas = content["Pistas"]
 	for p in pistas:
 		instantiate_pista(load_pista(p))
+	UiInterface.display_text(content['case_name'],5,1)
+	var poi = content["POI"]
+	UiInterface.load_map(poi)
+	var lr = content["LogicRelay"]
+	for p in lr:
+		case_logic_relay.append(p)
+		load_logic_relay(p)
+	var mx_areas = content['mx_areas']
+	for p in mx_areas:
+		case_mx_areas.append(p)
+		load_mx_gameplay_area(p)
+
+func load_non_persisten_data_case_file(case_name):
+	var jsonString
+	var file = File.new()
+	file.open('res://case_files/'+case_name+'.json',File.READ)
+	jsonString = file.get_as_text()
+	file.close()
+	
+	var content = JSON.parse(jsonString).result
+	
+	var mx_areas = content['mx_areas']
+	for p in mx_areas:
+		case_mx_areas.append(p)
+		load_mx_gameplay_area(p)
+	
+func try_load_mx_area():
+	for p in case_mx_areas:
+		load_mx_gameplay_area(p)
+	
+func load_mx_gameplay_area(dict):
+	var path = dict["location"]['location']
+	var target = get_node(path)
+	
+	## Não existe a cena agora
+	if not target:
+		return
+	## Esse nó ja existe?
+	if target.get_node(dict["name"]):
+		return
+	var instance = preload("res://Maps/Functional/mx_gameplay_area.tscn").instance()
+	instance.load_data(dict)
+	target.add_child(instance)
+
+func try_load_logic_relay():
+	for p in case_logic_relay:
+		load_logic_relay(p)
+
+func load_logic_relay(dict):
+	var path = dict["location"]
+	var target = get_node(path)
+	
+	## Não existe a cena agora
+	if not target:
+		return
+	## Esse nó ja existe?
+	if target.get_node(dict["name"]):
+		return
+	var instance = Node.new()
+	instance.set_script(LogicRelay)
+	instance.signals = dict["signals"]
+	instance.triggers = dict["triggers"]
+	instance.once = dict["once"]
+	instance.enabled = dict["enabled"]
+	instance.name = dict["name"]
+	target.add_child(instance)
+	
+func delete_logic_relay(reference):
+	if reference in case_logic_relay:
+		case_logic_relay.erase(reference)
 
 func load_npc(data):
 	var npc = npcObject.new()
@@ -150,13 +231,18 @@ func save_game():
 	for p in case_Pistas:
 		save_data["Pistas"].append(p.save_data())
 	
+	save_data["LogicRelay"] = []
+	for p in case_logic_relay:
+		save_data["LogicRelay"].append(p)
+	save_data["map_poi"] = UiInterface.save_map()
+	
 	var save_game = File.new()
 	save_game.open("user://savegame.save", File.WRITE)
 	save_game.store_line(to_json(save_data))
 	save_game.close()
 	
 func load_game():
-	
+	yield(UiInterface.fadein(),'fade_finished')
 	for c in get_node('/root/MainTree/WorldMapNode').get_children():
 		c.queue_free()
 		yield(c,'tree_exited')
@@ -164,6 +250,11 @@ func load_game():
 	for c in get_node('/root/MainTree/Interior').get_children():
 		c.queue_free()
 		yield(c,'tree_exited')
+	
+	case_NPCs = []
+	case_Pistas = []
+	case_logic_relay = []
+	case_mx_areas = []
 	
 	var save_game = File.new()
 	save_game.open("user://savegame.save", File.READ)
@@ -186,9 +277,20 @@ func load_game():
 	var npcs = saved_dict["NPC"]
 	for p in npcs:
 		instantiate_npc(load_npc(p))
+		
 	var pistas = saved_dict["Pistas"]
 	for p in pistas:
 		instantiate_pista(load_pista(p))
+	
+	var lr = saved_dict["LogicRelay"]
+	for p in lr:
+		case_logic_relay.append(p)
+		load_logic_relay(p)
+	
+	UiInterface.load_map(saved_dict["map_poi"])
+	
+	load_non_persisten_data_case_file(current_case_name)
+	UiInterface.fadeout()
 	
 func get_file_paths_in_folder(folder_path: String) -> Array:
 	
